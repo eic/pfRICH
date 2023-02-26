71b04b3d832bb5d53767cac11c82c0a5c8aac4c6
Chandradoy Chatterjee <chandradoy.chatterjee@ts.infn.it>
2023-02-26 12:48:40 +0100

    First step to automation
    The bash script runs the automation process.
    -  For testing purpose one can use the Geant4 visualization also for automation. 
    - The code can be used to generate default `HepMC3` file and run simulation and analysis.
    - User can define momentum and eta ranges (with specific bin size or entire range in one go)
    - User can switch off the Analysis and just run the simulation with the above two features.
    Currently the code is for two particles, should be sufficient for e-pi case. Soon a version will have handle all 4 particle cases.
    This makes modification in the HepMC3 writer.

---
+++ scripts/cc/hepmc_writer_two_particles_full.C
@@ -0,0 +1,84 @@
+//
+// export LD_LIBRARY_PATH=/home/eic/lib64:${LD_LIBRARY_PATH}
+//
+// root -l
+//
+// root [0] gSystem->AddIncludePath("-I/home/eic/include");
+// root [1] .x hepmc_writer_two_particles.C+("out.hepmc", 1000)
+//
+
+#include "HepMC3/GenEvent.h"
+#include "HepMC3/ReaderAscii.h"
+#include "HepMC3/WriterAscii.h"
+#include "HepMC3/Print.h"
+
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <math.h>
+
+#include <TMath.h>
+#include <TRandom.h>
+#include <TDatabasePDG.h>
+
+using namespace HepMC3;
+
+void hepmc_writer_two_particles_full(float pp1, float pp2, float et1, float et2, float pdg1, float pdg2, int n_events)
+{
+  auto *DatabasePDG = new TDatabasePDG();
+  auto *pion = DatabasePDG->GetParticle(pdg1), *kaon = DatabasePDG->GetParticle(pdg2);
+
+  WriterAscii hepmc_output("out.hepmc");
+  int events_parsed = 0;
+  GenEvent evt(Units::GEV, Units::MM);
+
+  // Random number generator
+  TRandom *rdmn_gen = new TRandom(0x12345678);
+
+  for (events_parsed = 0; events_parsed < n_events; events_parsed++) {
+    // type 4 is beam
+    GenParticlePtr p1 =
+        std::make_shared<GenParticle>(FourVector(0.0, 0.0, 12.0, 12.0), 11, 4);
+    GenParticlePtr p2 = std::make_shared<GenParticle>(
+        FourVector(0.0, 0.0, 100.0, 100.004), 2212, 4); 
+
+    GenVertexPtr v1 = std::make_shared<GenVertex>();
+    v1->add_particle_in(p1);
+    v1->add_particle_in(p2);
+
+    for(unsigned iq=0; iq</*2*/2; iq++) {
+      Double_t eta   = rdmn_gen->Uniform(et1, et2);
+      Double_t th    = 2*std::atan(exp(-eta));
+      Double_t p     = rdmn_gen->Uniform(pp1, pp2);
+      Double_t phi   = iq%2 ? 0.*M_PI/180. : 10.*M_PI/180.;
+
+      Double_t px    = p * std::cos(phi) * std::sin(th);
+      Double_t py    = p * std::sin(phi) * std::sin(th);
+      Double_t pz    = p * std::cos(th);
+
+      //auto particle = pion;//events_parsed%2 ? pion : kaon;
+      auto particle = iq%2 ? pion : kaon;
+      GenParticlePtr pq = std::make_shared<GenParticle>(FourVector(
+								   px, py, pz,
+								   sqrt(p*p + pow(particle->Mass(), 2))),
+							particle->PdgCode(), 1);
+      v1->add_particle_out(pq);
+    } //for iq
+
+    evt.add_vertex(v1);
+
+    if (events_parsed == 0) {
+      std::cout << "First event: " << std::endl;
+      Print::listing(evt);
+    }
+
+    hepmc_output.write_event(evt);
+    if (events_parsed % 10000 == 0) {
+      std::cout << "Event: " << events_parsed << std::endl;
+    }
+    evt.clear();
+  }
+  hepmc_output.close();
+  std::cout << "Events parsed and written: " << events_parsed << std::endl;
+  exit(0);
+}
---
+++ scripts/cc/multi-eval.C
@@ -0,0 +1,68 @@
+
+
+void multi_eval()
+{
+  //auto *reco = new ReconstructionFactory(dfname, cfname, "pfRICH", "BelleIIAerogel1");
+  //auto *reco = new ReconstructionFactory(dfname, cfname, "pfRICH", "Aerogel225");
+  const char * dfname = "pfrich.root";
+  const char * ofname = "output.root";
+  const char * cfname =0;
+  auto *reco = new ReconstructionFactory(dfname, cfname, "pfRICH");
+
+  // Factory configuration part;
+  //reco->IgnoreTimingInChiSquare();
+  //reco->SetSingleHitCCDFcut(0.005);
+  //reco->RemoveAmbiguousHits();
+  // Sensor active area pixelated will be pixellated NxN in digitization;
+  //reco->SetSensorActiveAreaPixellation(24);
+  // [rad] (should match SPE sigma) & [ns];
+  auto *a1 = reco->UseRadiator("Aerogel225", 0.0045);
+  auto *a2 = reco->UseRadiator("Aerogel155", 0.0045);
+  //reco->SetSinglePhotonTimingResolution(0.030);
+  //reco->SetQuietMode();
+  reco->AddHypothesis("pi+");
+  reco->AddHypothesis(321);
+
+  auto hmatch = new TH1D("hmatch", "PID evaluation correctness",       2,    0,      2);
+  //auto hthtr  = new TH1D("thtr",   "Cherenkov angle (track)",         80,  270,    310);
+  auto hthtr1  = new TH1D("thtr1",   "Cherenkov angle (track)",        200,  220,    320);
+  auto hthtr2  = new TH1D("thtr2",   "Cherenkov angle (track)",        200,  220,    320);
+
+  // This call is mandatory;
+  reco->PerformCalibration();
+  {
+    CherenkovEvent *event;
+
+    // Event loop;
+    while((event = reco->GetNextEvent())) {
+      for(auto mcparticle: event->ChargedParticles()) {
+	if (mcparticle->GetPDG() == mcparticle->GetRecoPdgCode()) {
+	  hmatch->Fill(0.5);
+	} else {
+	  hmatch->Fill(1.5);
+	} //if	  
+
+	hthtr1->Fill(1000*mcparticle->GetRecoCherenkovAverageTheta(a1));
+	hthtr2->Fill(1000*mcparticle->GetRecoCherenkovAverageTheta(a2));
+      } //for mcparticle
+    } //while
+  }
+
+  auto cv = new TCanvas("cv", "", 1600, 1000);
+  cv->Divide(4, 3);
+  cv->cd(1); reco->hthph()->Fit("gaus");
+  cv->cd(2); reco->hccdfph()->SetMinimum(0); reco->hccdfph()->Draw();
+  cv->cd(3); reco->hccdftr()->SetMinimum(0); reco->hccdftr()->Draw();
+  cv->cd(4); reco->hccdfev()->SetMinimum(0); reco->hccdfev()->Draw();
+  cv->cd(5); reco->hnpetr()->Draw();
+  cv->cd(6); reco->hmatch()->SetMinimum(0); reco->hmatch()->Draw();
+  cv->cd(7);       hmatch  ->SetMinimum(0);       hmatch  ->Draw();
+  cv->cd(8); reco->hdtph()->Fit("gaus");
+  cv->cd(9); hthtr1->Fit("gaus");
+  cv->cd(10); hthtr2->Fit("gaus");
+  TFile *f = new TFile(ofname,"RECREATE");
+  cv->SaveAs(ofname);
+  hthtr1->Write();
+  f->Close();
+  exit(0);
+} // multi_eval()
